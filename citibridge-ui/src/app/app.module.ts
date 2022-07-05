aa83f952f680c10e2961e55336e4cb786ffcf5d2
Sejal Kshirsagar <sejalsksagar@gmail.com>
2022-07-05 19:43:15 +0530

    Add frontend

---
+++ citibridge-ui/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TransactionComponent } from './transaction/transaction.component';
+import { TransactionlistComponent } from './transactionlist/transactionlist.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TransactionComponent,
+    TransactionlistComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
